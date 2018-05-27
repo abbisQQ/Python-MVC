d3ad1276f113fe3074b72aecdf493daf1a5dc908
Ch. Theodorou <theodoroucharalabos@gmail.com>
2018-05-27 16:07:56 +0300

    Add files via upload

---
+++ Controler.py
@@ -0,0 +1,63 @@
+import web
+from Models import RegisterModel, LoginModel
+
+web.config.debug = False
+
+urls = {
+    '/', 'Home',
+    '/register', 'Register',
+    '/login', 'Login',
+    '/logout', 'Logout',
+    '/postregistration', 'PostRegistration',
+    '/check-login', 'CheckLogin'
+    }
+
+
+app = web.application(urls, globals())
+session = web.session.Session(app, web.session.DiskStore("sessions"), initializer={'user': None})
+session_data = session._initializer
+
+
+render = web.template.render("Views/Templates", base="MainLayout", globals={'session': session_data, 'current_user': session_data["user"]})
+
+class Home:
+    def GET(self):
+        return render.Home()
+class Register:
+    def GET(self):
+        return render.Register()
+
+class Login:
+    def GET(self):
+        return render.Login()
+
+class PostRegistration:
+    def POST(self):
+        data = web.input()
+
+        req_model = ReqisterModel.RegisterModel()
+        reql_model.insert_user(data)
+        
+        return data.username
+
+class CheckLogin:
+    def POST(self):
+        data = web.input()
+        login = LoginModel.LoginModel()
+        isCorrect = login.check_user(data)
+
+        if isCorrect:
+            session_data["user"] = isCorrect
+            return isCorrect
+        
+        return "error"
+
+class Logout:
+    def GET(self):
+        session['user'] = None
+        session_data["user"] = None
+        session.kill()
+        return "success"
+
+if __name__ == "__main__":
+    app.run()
